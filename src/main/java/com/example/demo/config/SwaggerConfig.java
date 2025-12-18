1e953ad88394371ae18df62a35df580ea155a65b
bala-amypo <bala@amypo.in>
2025-12-18 09:00:22 +0000

    User commit

---
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.example.demo.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("SaaS User Role Permission Manager")
+                        .description("RBAC API documentation")
+                        .version("1.0"));
+    }
+}
