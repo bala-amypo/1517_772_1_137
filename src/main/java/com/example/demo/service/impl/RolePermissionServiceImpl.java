7f504a7113c349a539d5b784590e8d75cdd57a68
bala-amypo <bala@amypo.in>
2025-12-19 03:34:31 +0000

    User commit

---
+++ src/main/java/com/example/demo/service/impl/RolePermissionServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.RolePermission;
+import com.example.demo.repository.RolePermissionRepository;
+import com.example.demo.service.RolePermissionService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class RolePermissionServiceImpl implements RolePermissionService {
+
+    private final RolePermissionRepository repository;
+
+    public RolePermissionServiceImpl(RolePermissionRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public RolePermission grantPermission(RolePermission mapping) {
+        return repository.save(mapping);
+    }
+
+    @Override
+    public List<RolePermission> getAllMappings() {
+        return repository.findAll();
+    }
+
+    @Override
+    public RolePermission getMappingById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void revokePermission(Long id) {
+        repository.deleteById(id);
+    }
+}
---
+++ src/main/java/com/example/demo/service/impl/UserRoleServiceImpl.java
@@ -0,0 +1,38 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.entity.UserRole;
+import com.example.demo.repository.UserRoleRepository;
+import com.example.demo.service.UserRoleService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class UserRoleServiceImpl implements UserRoleService {
+
+    private final UserRoleRepository repository;
+
+    public UserRoleServiceImpl(UserRoleRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public UserRole assignRole(UserRole mapping) {
+        return repository.save(mapping);
+    }
+
+    @Override
+    public List<UserRole> getAllMappings() {
+        return repository.findAll();
+    }
+
+    @Override
+    public UserRole getMappingById(Long id) {
+        return repository.findById(id).orElseThrow();
+    }
+
+    @Override
+    public void removeRole(Long id) {
+        repository.deleteById(id);
+    }
+}
