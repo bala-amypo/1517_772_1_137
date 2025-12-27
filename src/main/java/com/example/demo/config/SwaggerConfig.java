4874ad3139ff11207b2d7c13c5cc85cb0b8411cf
bala-amypo <bala@amypo.in>
2025-12-27 05:22:54 +0000

    User commit

--- src/main/java/com/example/demo/config/SwaggerConfig.java
+++ src/main/java/com/example/demo/config/SwaggerConfig.java
@@ -1,20 +1,36 @@
 package com.example.demo.config;
-
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
 import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
 
     @Bean
-        public OpenAPI customOpenAPI() {
-                return new OpenAPI()
-                                
-                                                .servers(List.of(
-                                                                        new Server().url("https://9041.pro604cr.amypo.ai/")
-                                                                                        ));
-                                                                                                }
-                                                                                                }
+    public OpenAPI customOpenAPI() {
+
+        SecurityScheme bearerScheme = new SecurityScheme()
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT");
+
+        return new OpenAPI()
+                // Server
+                .servers(List.of(
+                        new Server().url("https://9041.pro604cr.amypo.ai/")
+                ))
+                // JWT Security
+                .components(new Components()
+                        .addSecuritySchemes("bearerAuth", bearerScheme)
+                )
+                .addSecurityItem(
+                        new SecurityRequirement().addList("bearerAuth")
+                );
+    }
+}
